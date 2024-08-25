d30327d1e2f935a10cd4a7496b79d3610df438ca
Ghina Shofa <ghinashofa@Ghinas-Macbook.local>
2024-08-25 11:37:07 +0700

    Slicing UI: Sidebar & Cards Dashboard'

---
+++ budgetbuddy/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom-combined': '0px 18px 27px rgba(0, 0, 0, 0.06), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 0px 1px rgba(0, 0, 0, 0.04)',
+      },
+    },
+  },
+  plugins: [],
+}
